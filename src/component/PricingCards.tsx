101ded59aa50e956d6e98fca929bfd41159ca9ad
Folacodes <folacodes@Folacodess-MacBook-Pro.local>
2025-01-16 21:51:52 +0100

    changes to pricing plan for test

--- src/component/PricingCards.tsx
+++ src/component/PricingCards.tsx
@@ -128,7 +128,7 @@ const PricingCard = ({ planName, amount }: PricingCardProp) => {
         disabled={createPaymentMutation.isPending}
         className="text-white bg-primary hover:bg-green_300 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
       >
-        Choose plan
+        Choose planss
       </CustomButton>
 
       {referenceNumber !== "" && (
